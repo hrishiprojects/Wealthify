dab295537c1b246b3ccd52b72ecbfe2943f31d6c
suhail <suhail@suhails-MacBook-Air.local>
2024-07-23 01:38:07 +0530

    resolved issue

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,18 +1,20 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { FetchBaseQueryError, createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-
+import { QueryReturnValue } from "@reduxjs/toolkit/dist/query/baseQueryTypes";
 
 export const api = createApi({
-    baseQuery : fetchBaseQuery({ baseUrl : import.meta.env.VITE_BASE_URL}),
-    reducerPath:"main",
-    tagTypes: ["Kpis"],
-    endpoints : (build) => ({
-        getKpis : build.query<void,void>({
-            query:()=> "kpi/kpis/",
-            providesTags:["Kpis"]
-        }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis"],
+  endpoints: (build) => ({
+    getKpis: build.query<void, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
     }),
+  }),
 });
 
-export const { useGetKpisQuery } = api;
+export const {
+  useGetKpisQuery,
+}: { useGetKpisQuery: () => QueryReturnValue<void, FetchBaseQueryError, {}> } =
+  api;
